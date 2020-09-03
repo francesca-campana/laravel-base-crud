ce0f4730e901adb367da817fb8601b37e891bb23
francesca-campana <franx3rox@gmail.com>
2020-09-03 20:37:24 +0200

    Versione 0.5
    Delete&Destroy

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,17 +1,26 @@
-<h1>Movies list</h1>
-@foreach ($movies as $movie)
-  <div>
-    <h2>
-      <a href="{{ route('movies.show', $movie->id) }}"> Titolo: {{ $movie->title }}
-        <span>
-          <a href="{{ route('movies.edit', $movie->id) }}"> - Modifica</a>
-        </span>
-      </a>
-    </h2>
-  </div>
-@endforeach
-<ul>
-  <li>
-    <a href="{{ route('movies.create', $movie->id) }}">Add new movie</a>
-  </li>
-</ul>
+<div>
+  <h1>Movies list</h1>
+  <ul>
+    @foreach ($movies as $movie)
+      <li>{{ $movie->title }}
+        <a href="{{ route('movies.show', $movie->id) }}">Dettagli</a>
+        <a href="{{ route('movies.edit', $movie->id) }}"> Modifica</a>
+
+        <form action="{{ route('movies.destroy', $movie->id) }}" method="post">
+          @csrf
+          @method('DELETE')
+          <input type="submit" value="Elimina">
+
+        </form>
+      </li>
+
+    @endforeach
+  </ul>
+
+  <ul>
+    <li>
+      <a href="{{ route('movies.create', $movie->id) }}">Add new movie</a>
+    </li>
+  </ul>
+
+</div>
